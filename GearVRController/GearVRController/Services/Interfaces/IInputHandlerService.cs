a554df1ea4aecf6f140082ca5e145af32b7731b1
shiso <shisoratsu@icloud.com>
2025-12-30 20:42:08 +0800

    refactor: drop legacy C# implementation, migration to Rust complete

--- GearVRController/GearVRController/Services/Interfaces/IInputHandlerService.cs
+++
@@ -1,9 +0,0 @@
-using GearVRController.Models;
-
-namespace GearVRController.Services.Interfaces
-{
-    public interface IInputHandlerService
-    {
-        void ProcessInput(ControllerData data);
-    }
-}
